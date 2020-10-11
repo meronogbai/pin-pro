739fd68c4959b797e8862de17f361df1440d0dc0
Meron Ogbai <okbaymeron@gmail.com>
2020-10-11 21:17:53 +0300

    Add pin function

--- main.rb
+++ main.rb
@@ -5,3 +5,9 @@ require 'discordrb'
 
 bot = Discordrb::Commands::CommandBot.new token: ENV['TOKEN'], client_id: 168_123_456_789_123_456, prefix: '%'
 
+users = Hash.new([])
+
+bot.command :pin do |event, *text|
+  users[event.user].push(*text.join(' '))
+  event.respond "\"#{text.join(' ')}\" has been pinned!"
+end
